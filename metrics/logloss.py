34c65da542eaaf4d5a5807d60bcdd63beeba6588
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2025-01-23 14:06:48 +0530

    fix clipping

--- metrics/logloss.py
+++ metrics/logloss.py
@@ -17,7 +17,7 @@ def log_loss(y_true, y_pred, threshold_range=None):
       The computed metric.
     """
     if threshold_range is not None:
-      y_pred = np.clip(y_pred, threshold_range[0], threshold_range[1])
+      y_true = np.clip(y_true, threshold_range[0], threshold_range[1])
     return np.mean(np.log(1 - np.abs(np.array(y_true) - np.array(y_pred))))
 
 log_loss_scorer = make_scorer(log_loss, greater_is_better=True)
