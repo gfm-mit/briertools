5608d09c19697bb08d3b927a7212de32b5814b54
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2025-01-27 11:20:26 +0530

    Update paper_examples.py

--- tests/paper_examples.py
+++ tests/paper_examples.py
@@ -15,7 +15,9 @@ def jail():
 
   y_pred, y_true = simulate_binormal(3, 1)
   log_loss_curve(y_true, y_pred, threshold_range=(0.003, 0.66), fill_range=(1./101, 1./6), ticks=[1./101, 1./6, 1./2])
+
   plt.show()
+  plt.tight_layout()
 
 def fraud():
   y_pred, y_true = simulate_binormal(1, 1)
@@ -23,7 +25,9 @@ def fraud():
 
   y_pred, y_true = simulate_binormal(1, 2)
   log_loss_curve(y_true, y_pred, threshold_range=(0.333, 0.9995), fill_range=(100./101, 1000./1001), ticks=[1./2, 100./101, 1000./1001])
+
   plt.show()
+  plt.tight_layout()
 
 def cancer():
   y_pred, y_true = simulate_binormal(1, 1)
@@ -33,5 +37,6 @@ def cancer():
   log_loss_curve(y_true, y_pred, threshold_range=(0.03, 0.66), fill_range=(1./11, 1./3), ticks=[1./11, 1./3, 1./2])
 
   plt.show()
+  plt.tight_layout()
 
 cancer()
