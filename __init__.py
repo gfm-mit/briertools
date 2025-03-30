b56163c13ad545b0da743bdd949898f739131b80
Alyssa Smith <asmithh99@gmail.com>
2025-03-29 22:23:27 -0400

    restructure to be more pypi compatible

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .briertools.scorers import DCAScorer, LogLossScorer, BrierScorer
-
-__all__ = ["DCAScorer", "LogLossScorer", "BrierScorer"]
---
+++ briertools/__init__.py
@@ -0,0 +1,3 @@
+from .scorers import DCAScorer, LogLossScorer, BrierScorer
+
+__all__ = ["DCAScorer", "LogLossScorer", "BrierScorer"]
